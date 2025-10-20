ab98327c34083d49dba47ea436251fd07d80816f
saber <3486188492@qq.com>
2025-10-20 21:55:40 +0800

    feat(index): 为主页添加了样式 为分区功能栏添加了样式，并且引入了png图标进行美化 给内容区域的主标题添加了样式 引入的图片文件位置在：/static/icon

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -55,23 +55,58 @@
         </view>
         <!-- 分区功能栏 -->
         <view class="function-buttons">
-          <wd-grid clickable>
-            <wd-grid-item icon="picture" text="文字" />
-            <wd-grid-item icon="picture" text="文字" />
-            <wd-grid-item icon="picture" text="文字" />
+          <wd-grid square :gutter="18" clickable custom-class="custom-grid" :column="4">
+            <wd-grid-item text="二手好物" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon1" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="课表信息" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon2" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="校内点餐" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon3" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="校内点餐" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon3" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="二手好物" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon1" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="课表信息" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon2" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="校内点餐" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon3" />
+              </template>
+            </wd-grid-item>
+            <wd-grid-item text="校内点餐" custom-class="custom-item">
+              <template #icon>
+                <wd-img :width="48" :height="48" :src="icon3" />
+              </template>
+            </wd-grid-item>
           </wd-grid>
         </view>
       </view>
       <!-- 推荐内容 -->
       <view class="content">
-        <view class="title"> </view>
-        <view class="posts">
+        <view class="title">
           <wd-row>
-            <wd-col :span="8" :offset="2"><view class="bg-dark1">更多为你推荐</view></wd-col>
-            <wd-col :span="2" :offset="8"
-              ><view class="bg-dark1"><wd-button>更多></wd-button></view></wd-col
-            >
+            <wd-col :span="24"><view>更多为你推荐</view></wd-col>
           </wd-row>
+        </view>
+        <view class="posts">
           <ul>
             <li>
               <PostsCard></PostsCard>
@@ -97,6 +132,9 @@ const menuButtonBounding = ref(null)
 const statusBarHeight = ref(0)
 
 const joy = ref('https://wot-ui.cn/assets/redpanda.jpg')
+const icon1 = ref('/static/icon/shopping.png')
+const icon2 = ref('/static/icon/schedule.png')
+const icon3 = ref('/static/icon/catering.png')
 
 onMounted(() => {
   testApi()
@@ -205,6 +243,22 @@ function getMenuButtonAlignedStyle() {
   }
   :deep(.custom-image-prev) {
     height: 168px !important;
+  }
+}
+.function-buttons {
+  :deep(.custom-grid) {
+    padding-bottom: 0 !important;
+  }
+  :deep(.custom-item .wd-grid-item__content) {
+    border-radius: 12px;
+    .wd-grid-item__text {
+      margin: 0;
+    }
+  }
+}
+.content {
+  .title {
+    margin: 0 20px 10px;
   }
 }
 </style>
