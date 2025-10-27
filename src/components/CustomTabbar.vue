bd37ae86ee2ba03f547662e9cfc27ca7d24acf54
saber <3486188492@qq.com>
2025-10-27 20:06:09 +0800

    fix(CustomTabbar): 修复底部导航栏遮挡 -添加了占位属性

--- src/components/CustomTabbar.vue
+++ src/components/CustomTabbar.vue
@@ -1,6 +1,13 @@
 <template>
   <view class="tabbar">
-    <wd-tabbar v-model="tabbar" shape="round" fixed :safeAreaInsetBottom="true" bordered>
+    <wd-tabbar
+      v-model="tabbar"
+      shape="round"
+      fixed
+      :safeAreaInsetBottom="true"
+      bordered
+      placeholder
+    >
       <wd-tabbar-item title="首页" icon="home"></wd-tabbar-item>
       <wd-tabbar-item title="分类" icon="cart"></wd-tabbar-item>
       <wd-tabbar-item title="我的" icon="user"></wd-tabbar-item>
