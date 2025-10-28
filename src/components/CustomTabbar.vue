a00756326136a23f269472de05505885ed2f29ec
saber <3486188492@qq.com>
2025-10-28 18:03:06 +0800

    feat(Mine): 添加了“我的”页面 添加了个人简介卡片 添加了个人数据卡片 添加了个人交易卡片

--- src/components/CustomTabbar.vue
+++ src/components/CustomTabbar.vue
@@ -7,14 +7,41 @@
       :safeAreaInsetBottom="true"
       bordered
       placeholder
+      @change="changeTabbar"
     >
       <wd-tabbar-item title="首页" icon="home"></wd-tabbar-item>
-      <wd-tabbar-item title="分类" icon="cart"></wd-tabbar-item>
+      <wd-tabbar-item title="消息" icon="chat"></wd-tabbar-item>
+      <wd-tabbar-item title="订单" icon="cart"></wd-tabbar-item>
       <wd-tabbar-item title="我的" icon="user"></wd-tabbar-item>
     </wd-tabbar>
   </view>
 </template>
 <script setup>
 import { ref } from 'vue'
-const tabbar = ref(1)
+const tabbar = ref(0)
+function changeTabbar(value) {
+  const index = value.value
+  switch (index) {
+    case 0:
+      uni.switchTab({
+        url: '/pages/index/index'
+      })
+      break
+    case 1:
+      console.log(index)
+
+      break
+    case 2:
+      console.log(index)
+
+      break
+    case 3:
+      uni.switchTab({
+        url: '/pages/Mine/MineIndex'
+      })
+      break
+    default:
+      break
+  }
+}
 </script>
