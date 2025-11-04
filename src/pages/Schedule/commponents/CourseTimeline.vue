d6d9003954101364819c1278cda265fd4bbb57fb
saber <3486188492@qq.com>
2025-11-04 19:20:43 +0800

    feat(Schedule): 完善课程时间轴组件功能和样式

--- src/pages/Schedule/commponents/CourseTimeline.vue
+++ src/pages/Schedule/commponents/CourseTimeline.vue
@@ -2,12 +2,13 @@
   <view class="course-timeline">
     <wd-steps vertical>
       <wd-step v-for="(course, index) in courses" :key="index">
+        <!-- 时间 -->
         <template #icon>
-          <view style="display: flex; align-items: center; justify-content: center">
+          <view class="time">
             <wd-text :text="course.time" color="#333" size="18px" bold></wd-text>
           </view>
         </template>
-        <template #title></template>
+        <!-- 课程卡片 -->
         <template #description>
           <view class="course-card">
             <wd-card custom-class="card" :custom-style="`background: ${course.color}` + '36'">
@@ -16,10 +17,10 @@
               </template>
               <template #footer>
                 <view class="other-info">
-                  <wd-text text="老师：" :color="course.color" />
-                  <wd-text :text="course.teacher" :color="course.color" />
-                  <wd-text text=" | 教室" :color="course.color" />
-                  <wd-text :text="course.classroom" :color="course.color" />
+                  <wd-text
+                    :text="`老师：${course.teacher} | 教室:${course.classroom}`"
+                    :color="course.color"
+                  />
                 </view>
               </template>
             </wd-card>
@@ -30,39 +31,34 @@
   </view>
 </template>
 <script setup>
-defineOptions({
-  options: {
-    styleIsolation: 'shared'
-  }
-})
 import { ref } from 'vue'
 
 // 课表颜色方案 - 使用8位十六进制格式表示透明度
 const courseColors = [
-  '#e9805b', // 橙色
-  '#a6c3ad', // 绿色
-  '#8aabd6', // 蓝色
-  '#d4a2c9', // 粉色
-  '#9acbd6', // 青色
-  '#e6b8a2', // 浅橙色
-  '#b39ddb', // 紫色
-  '#a5d6a7', // 浅绿色
-  '#ffcc80', // 浅黄色
-  '#f48fb1' // 浅粉色
+  '#e9805b',
+  '#a6c3ad',
+  '#8aabd6',
+  '#d4a2c9',
+  '#9acbd6',
+  '#e6b8a2',
+  '#b39ddb',
+  '#a5d6a7',
+  '#ffcc80',
+  '#f48fb1'
 ]
 
 // 课表背景色方案 - 使用8位十六进制格式表示透明度（20%透明度）
 const courseBackgroundColors = [
-  '#e9805b20', // 橙色背景
-  '#a6c3ad20', // 绿色背景
-  '#8aabd620', // 蓝色背景
-  '#d4a2c920', // 粉色背景
-  '#9acbd620', // 青色背景
-  '#e6b8a220', // 浅橙色背景
-  '#b39ddb20', // 紫色背景
-  '#a5d6a720', // 浅绿色背景
-  '#ffcc8020', // 浅黄色背景
-  '#f48fb120' // 浅粉色背景
+  '#e9805b20',
+  '#a6c3ad20',
+  '#8aabd620',
+  '#d4a2c920',
+  '#9acbd620',
+  '#e6b8a220',
+  '#b39ddb20',
+  '#a5d6a720',
+  '#ffcc8020',
+  '#f48fb120'
 ]
 // 课程信息数据
 const courses = ref([
@@ -107,10 +103,20 @@ const courses = ref([
     classroom: '操场'
   }
 ])
+defineOptions({
+  options: {
+    styleIsolation: 'shared'
+  }
+})
 </script>
 <style scoped lang="scss">
 .course-timeline {
   padding: 0 20px;
+  .time {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
   .course-card {
     :deep(.card) {
       padding: 10px 20px;
